2f2fb77883c2fd7cc23eab291321c8a1e4de7377
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-09-25 00:10:19 -0400

    initial commit

---
+++ react-fb-clientpanel/README.md
@@ -0,0 +1,17 @@
+### This a react project from Brad Traversy's Udemy Course
+
+It uses react, redux and firebase(for storage and authentication). Its a Client Panel Application where users can login and add balance of how much others owe them etc. Here are some of its images. 
+
+* Login/Register Page ![Login](https://user-images.githubusercontent.com/18495886/45992092-efa32600-c055-11e8-9fea-fa1bbcce3e23.png)
+* Dashboard section ![Dashboard](https://user-images.githubusercontent.com/18495886/45992134-21b48800-c056-11e8-9fc3-11fe79208c47.png)
+* Individual Client section ![ClientDetails](https://user-images.githubusercontent.com/18495886/45992143-2aa55980-c056-11e8-9b69-2913557452c5.png)
+* Client Add/Edit section ![Add/Edit Client](https://user-images.githubusercontent.com/18495886/45992146-2c6f1d00-c056-11e8-9cf5-7384190f391f.png)
+* Settings Section ![Settings](https://user-images.githubusercontent.com/18495886/45992148-2da04a00-c056-11e8-9fa9-1f68277ad45f.png)
+
+---
+
+### To run locally
+* Git clone
+* npm install
+* Add your firebase details to config/FirebaseConfig.js
+* npm start and go to localhost:3000
---
+++ react-fb-clientpanel/src/store.js
@@ -0,0 +1,65 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import { firebaseConfig } from './config/FirebaseConfig';
+
+// Reducers - todo
+import notifyReducer from './reducers/notifyReducer';
+import settingsReducer from './reducers/settingsReducer';
+
+// react redux firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of realtime DB
+};
+
+// Init firebase instance
+firebase.initializeApp(firebaseConfig);
+// Init firestore instance
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase) // <- needed if using firestore
+)(createStore);
+
+// Add firebase to rootreducer
+const rootreducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
+  notify: notifyReducer,
+  settings: settingsReducer
+});
+
+// Check for settings in localStorage
+if (localStorage.getItem('settings') == null) {
+  // Default settings
+  const defaultSettings = {
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: false,
+    allowRegistration: false
+  };
+
+  // Set to localStorage
+  localStorage.setItem('settings', JSON.stringify(defaultSettings));
+}
+
+// Create initial state
+const initialState = { settings: JSON.parse(localStorage.getItem('settings')) };
+
+// Create store
+const store = createStoreWithFirebase(
+  rootreducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
