8b276ad940881e78b613d8116ecc9e4aae4dc031
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-08 20:54:16 -0400

    Completed todo functionality. Todo: split into components and style using css

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import classNames from 'classnames';
 
 class App extends Component {
   state = {
     newTodo: '',
+    allDone: false,
     todos: [
       {
         title: 'Learn Java',
@@ -41,17 +43,51 @@ class App extends Component {
     this.setState({ newTodo: e.target.value });
   };
 
+  tickChanged = (e, index) => {
+    const todos = [...this.state.todos]; // performs only a shallow copy
+    todos[index] = { ...todos[index], done: e.target.checked }; // copy checked value into this object
+
+    this.setState({ todos }); // or todos:todos
+  };
+
+  delete = index => {
+    const todos = [...this.state.todos]; // performs only a shallow copy
+    todos.splice(index, 1);
+    this.setState({ todos }); // or todos:todos
+  };
+
+  removeAll = () => {
+    this.setState({ todos: [] });
+  };
+
+  allDone = () => {
+    const todos = [...this.state.todos];
+    todos.forEach((todo, index) => (todos[index] = { ...todos[index], done: !this.state.allDone }));
+    this.setState({ todos, allDone: !this.state.allDone });
+  };
+
   render() {
     return (
       <div className="container">
         <form onSubmit={event => this.formSubmitted(event)}>
           <label htmlFor="title">Todo Title: </label>
           <input type="text" onChange={e => this.nameChanged(e)} value={this.state.newTodo} />
-          <button type="submit">Submit</button>
+          <button type="submit">Add Todo</button>
         </form>
+        <button onClick={this.allDone}>{this.state.allDone ? 'Open All Todos' : 'Finish All Todos'}</button>
+        <button onClick={this.removeAll}>Remove all</button>
         <ul>
-          {this.state.todos.map(todo => {
-            return <li key={todo.title}>{todo.title}</li>;
+          {this.state.todos.map((todo, index) => {
+            return (
+              <li key={todo.title} className={classNames({ strikethrough: todo.done })}>
+                <input
+                  type="checkbox"
+                  onChange={e => this.tickChanged(e, index)}
+                  checked={this.state.todos[index].done}
+                />
+                {todo.title} <i className="fas fa-times-circle" onClick={this.delete.bind(this, index)} />
+              </li>
+            );
           })}
         </ul>
       </div>
