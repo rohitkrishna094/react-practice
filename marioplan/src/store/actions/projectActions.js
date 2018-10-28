f50e212f1882f2bb0eb5142217907acb8816647a
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-27 21:25:49 -0400

    Added data to firestore

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -2,11 +2,23 @@
 // a function. The advantage of this is that we can now halt
 // the flow and run async code just before we go to reducer
 export const createProject = project => {
-  return (dispatch, getState) => {
-    // make async call to db
-    // do async
-
-    // and now dispatch it back
-    dispatch({ type: 'CREATE_PROJECT', project: project });
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    // make async call to db do async
+    const firestore = getFirestore();
+    firestore
+      .collection('projects')
+      .add({
+        ...project,
+        authorFirstName: 'Rohit',
+        authorLastName: 'Krishna',
+        createdAt: new Date()
+      })
+      .then(() => {
+        // and now dispatch it back
+        dispatch({ type: 'CREATE_PROJECT', project: project });
+      })
+      .catch(err => {
+        dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+      });
   };
 };
