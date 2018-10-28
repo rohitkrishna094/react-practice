266b73f326e4a775f8acf7bab0feeb3280154103
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-27 23:48:01 -0400

    added firebase auth

--- marioplan/src/components/auth/SignIn.js
+++ marioplan/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 
 class SignIn extends Component {
   state = { email: '', password: '' };
@@ -9,10 +11,11 @@ class SignIn extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
 
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -29,6 +32,7 @@ class SignIn extends Component {
 
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">{authError ? <p>{authError}</p> : null}</div>
           </div>
         </form>
       </div>
@@ -36,4 +40,19 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: creds => dispatch(signIn(creds))
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
