01abc2a24c61e80d23b217639302251aa6389d11
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-28 02:10:21 -0400

    Added user profile data

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -16,7 +16,11 @@ const store = createStore(
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+    reactReduxFirebase(fbConfig, {
+      attachAuthIsReady: true,
+      useFirestoreForProfile: true,
+      userProfile: 'users'
+    })
   )
 );
 
